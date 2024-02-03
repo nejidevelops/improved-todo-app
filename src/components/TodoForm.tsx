a33e262e80a2b6887d0781b6f607cb44cdec275f
Newton Ombese <newtonombese1@gmail.com>
2024-02-03 08:42:09 +0300

    Setting up the todoForm component

---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function TodoForm() {
+  return (
+    <>
+      <form>
+        <input type="text" placeholder='Enter a todo activity' />
+        <button type="submit">Add</button>
+      </form>
+    </>
+  )
+}
+
+export default TodoForm
