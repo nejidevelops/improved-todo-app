9b39ecf9d1ccacdf3f783637351d4930caba3fba
Newton Bofulo <newtonombese1@gmail.com>
2024-02-23 21:03:30 +0300

    Setting up the add todo logic

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function TodoForm() {
+  const [todo, setTodo] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(todo);
+    setTodo("");
+  }
+
   return (
     <>
-      <form>
-        <input type="text" placeholder='Enter a todo activity' />
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} placeholder='Enter a todo activity' />
         <button type="submit">Add</button>
       </form>
     </>
